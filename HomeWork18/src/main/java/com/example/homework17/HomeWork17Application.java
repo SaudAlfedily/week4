08558da73d0f1e9903b7a237f9459a333102de5d
SaudAlfedily <132199446+SaudAlfedily@users.noreply.github.com>
2023-05-23 14:38:05 +0300

    Add files via upload

---
+++ HomeWork18/src/main/java/com/example/homework17/HomeWork17Application.java
@@ -0,0 +1,14 @@
+package com.example.homework17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork17Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HomeWork17Application.class, args);
+    }
+
+}
